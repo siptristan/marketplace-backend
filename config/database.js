b0da3c28b9eb279f8b5c6bf73b462f6ac6d7f279
siptristan <sip.tristan@hotmail.com>
2023-03-18 15:14:24 +0700

    change mysql to environment variable

--- config/database.js
+++ config/database.js
@@ -6,9 +6,9 @@ const mysql = require('mysql');
 
 const db = mysql.createConnection({
     host: process.env.MYSQL_HOST,
-    user: 'root',
-    password: '',
-    database: 'marketplace'
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 });
 
 //connect ke database
