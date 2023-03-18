03e588fbc64355be92f1abd84179f3a8e7208884
siptristan <sip.tristan@hotmail.com>
2023-03-18 17:04:49 +0700

    fix case sensitive

--- model/product_model.js
+++ model/product_model.js
@@ -1,12 +1,12 @@
 module.exports = {
     product: (UserID) => {
-        let sql = `SELECT p.*, c.CategoryID, c.CategoryName FROM products p JOIN Categories c ON p.CategoryID = c.CategoryID ${UserID !== undefined ? `WHERE p.UserID != ${UserID} AND` : 'WHERE'} p.IsDeleted = 0`;
+        let sql = `SELECT p.*, c.CategoryID, c.CategoryName FROM products p JOIN categories c ON p.CategoryID = c.CategoryID ${UserID !== undefined ? `WHERE p.UserID != ${UserID} AND` : 'WHERE'} p.IsDeleted = 0`;
         return sql;
     },
     category: () => {
         return "SELECT * FROM categories";
     },
     productById: (slug) => {
-        return `SELECT p.*, c.CategoryID, c.CategoryName FROM products p JOIN Categories c ON p.CategoryID = c.CategoryID WHERE Slug = '${slug}'`;
+        return `SELECT p.*, c.CategoryID, c.CategoryName FROM products p JOIN categories c ON p.CategoryID = c.CategoryID WHERE Slug = '${slug}'`;
     }
 }
